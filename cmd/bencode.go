a62ddd9e5ede6749e832fc7c2427cf416c419b36
ze0s <ze0s@riseup.net>
2023-03-24 19:26:33 +0100

    feat: bencode edit

---
+++ cmd/bencode.go
@@ -0,0 +1,137 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"migraterr/internal/bencoding"
+
+	"github.com/spf13/cobra"
+)
+
+func RunBencode() *cobra.Command {
+
+	command := &cobra.Command{
+		Use:   "bencode",
+		Short: "bencoded tools",
+		Long:  `bencoded tools for editing and showing files like .torrents and fastresume`,
+		Example: `  migraterr bencode 
+  migraterr bencode --help`,
+		SilenceUsage: true,
+	}
+
+	command.RunE = func(cmd *cobra.Command, args []string) error {
+		return cmd.Usage()
+	}
+
+	command.AddCommand(RunBencodeEdit())
+	command.AddCommand(RunBencodeInfo())
+
+	return command
+}
+
+func RunBencodeEdit() *cobra.Command {
+
+	command := &cobra.Command{
+		Use:   "edit",
+		Short: "Edit bencode files",
+		Long:  `Edit bencoded files like .torrents and fastresume`,
+		Example: `  migraterr bencode edit
+  migraterr bencode edit --help`,
+		SilenceUsage: true,
+	}
+
+	var (
+		dry          bool
+		verbose      bool
+		export       string
+		glob         string
+		replacements []string
+	)
+
+	command.Flags().BoolVar(&dry, "dry-run", false, "Dry run, don't write changes")
+	command.Flags().BoolVarP(&verbose, "verbose", "v", false, "Verbose output")
+
+	command.Flags().StringVar(&glob, "glob", "", "Glob to files: eg ./files/**/*.torrent.rtorrent")
+	command.Flags().StringVar(&export, "export", "", "Export to directory. Will replace if not specified")
+	command.Flags().StringSliceVar(&replacements, "replace", []string{}, "Replace: pattern|replace")
+
+	command.Run = func(cmd *cobra.Command, args []string) {
+		if glob == "" {
+			log.Fatal("must have dir\n")
+		}
+
+		if len(replacements) == 0 {
+			log.Fatal("must supply replacements\n")
+		}
+
+		processedFiles := 0
+
+		files, err := filepath.Glob(glob)
+		if err != nil {
+			log.Fatal("could not open files\n")
+		}
+
+		for _, file := range files {
+			fmt.Printf("found: %s", file)
+			processedFiles++
+
+			_, fileName := filepath.Split(file)
+
+			if export != "" {
+				export = filepath.Join(export, fileName)
+			}
+
+			if err := bencoding.Process(file, export, replacements, verbose); err != nil {
+				log.Fatalf("error processing file: %v", err)
+			}
+
+			if verbose {
+				log.Printf("[%d/%d] sucessfully processed file %s", len(files), processedFiles, fileName)
+			}
+		}
+
+		fmt.Printf("processed %d files\n", processedFiles)
+	}
+
+	return command
+}
+
+func RunBencodeInfo() *cobra.Command {
+
+	command := &cobra.Command{
+		Use:   "info",
+		Short: "Info bencode files",
+		Long:  `Info bencoded files like .torrents and fastresume`,
+		Example: `  migraterr bencode info
+  migraterr bencode info --help`,
+		SilenceUsage: true,
+	}
+
+	var glob string
+
+	command.Flags().StringVar(&glob, "glob", "", "Glob to files: eg ./files/**/*.torrent.rtorrent")
+
+	command.Run = func(cmd *cobra.Command, args []string) {
+		if glob == "" {
+			log.Fatal("must have dir\n")
+		}
+
+		files, err := filepath.Glob(glob)
+		if err != nil {
+			log.Fatal("could not open files\n")
+		}
+
+		fmt.Printf("found %d files\n", len(files))
+
+		for _, file := range files {
+			if err := bencoding.Info(file); err != nil {
+				log.Fatalf("error reading file: %s  %v", file, err)
+			}
+		}
+
+	}
+
+	return command
+}
---
+++ internal/bencoding/bencode.go
@@ -0,0 +1,114 @@
+package bencoding
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/zeebo/bencode"
+)
+
+func Process(path, export string, replacements []string, verbose bool) error {
+	read, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("error reading file: %s err: %q", path, err)
+	}
+
+	var fastResume map[string]interface{}
+	if err := bencode.DecodeString(string(read), &fastResume); err != nil {
+		log.Printf("could not decode bencode file: %s", path)
+	}
+
+	if len(replacements) > 0 {
+		for k, v := range fastResume {
+			for _, replacement := range replacements {
+				if !strings.Contains(replacement, "|") {
+					continue
+				}
+
+				parts := strings.Split(replacement, "|")
+
+				if len(parts) == 0 || len(parts) > 2 {
+					continue
+				}
+
+				switch val := v.(type) {
+				case string:
+					if strings.Contains(val, parts[0]) {
+						fastResume[k] = strings.Replace(val, parts[0], parts[1], -1)
+					}
+				default:
+					continue
+				}
+
+				if verbose {
+					fmt.Printf("replaced: '%s' with '%s'\n", parts[0], parts[1])
+				}
+			}
+		}
+	}
+
+	if export != "" {
+		if err = Encode(export, fastResume); err != nil {
+			log.Printf("could not export fastresume file %s error: %q", path, err)
+			return err
+		}
+	} else {
+		if err = Encode(path, fastResume); err != nil {
+			log.Printf("could not write fastresume file %s error: %q", path, err)
+			return err
+		}
+	}
+
+	log.Printf("sucessfully processed file %s", path)
+
+	return nil
+}
+
+func Encode(path string, data any) error {
+	file, err := os.Create(path)
+	if err != nil {
+		log.Printf("os create error: %q", err)
+		return err
+	}
+
+	defer file.Close()
+
+	bufferedWriter := bufio.NewWriter(file)
+	encoder := bencode.NewEncoder(bufferedWriter)
+	if err := encoder.Encode(data); err != nil {
+		log.Printf("encode error: %q", err)
+		return err
+	}
+
+	if err := bufferedWriter.Flush(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Info(path string) error {
+	read, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("error reading file: %s err: %q", path, err)
+	}
+
+	var fastResume map[string]interface{}
+	if err := bencode.DecodeString(string(read), &fastResume); err != nil {
+		log.Printf("could not decode bencode file %s", path)
+	}
+
+	_, fileName := filepath.Split(path)
+
+	fmt.Printf("Filename: %s\n", fileName)
+	for k, v := range fastResume {
+		fmt.Printf("%s: %v\n", k, v)
+	}
+	fmt.Println("")
+
+	return nil
+}
